983db6e76ecda7f286d2a4825861f7175d0b08f0
Wikum Chamith <wikum@linuxdeveloper.space>
2023-06-11 10:44:58 +0530

    Add a method to BookingService

--- src/main/java/com/example/backend/service/BookingService.java
+++ src/main/java/com/example/backend/service/BookingService.java
@@ -1,5 +1,6 @@
 package com.example.backend.service;
 
+import com.example.backend.model.Booking;
 import com.example.backend.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,9 @@ public class BookingService {
 
 	@Autowired
 	private BookingRepository bookingRepository;
+
+	public void addBooking(Booking booking) {
+
+	}
 
 }
